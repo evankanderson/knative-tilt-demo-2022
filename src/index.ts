82a0d3c5bbb616223c0fdf2f7081bc629632e95e
Evan Anderson <evana@vmware.com>
2022-02-23 04:33:19 -0800

    Update configuration to run `react start` as well as `nodemon`

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,16 @@ function listDocs(req: Request, res: Response) {
   res.json({
     items: [
       {name: 'One', count: 3},
-      {name: 'Two', count: 1},
-      {name: 'Win', count: 42},
-      {name: 'Will', count: 0},
-      {name: 'This', count: -1},
-      {name: 'overflow?', count: 2},
+      // {name: 'Two', count: 1},
+      // {name: 'Will', count: 0},
+      // {name: 'This', count: -1},
+      // {name: 'overflow?', count: 2},
     ],
   });
 }
 
 function main() {
-  const PORT: string | number = process.env.PORT || 8080;
+  const PORT: string | number = 8080; // process.env.PORT || 8080
 
   // Serve the React static files after build
   app.use(express.static('client/build'));
